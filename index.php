e08697a81ae7ba3a601a62fdd9450da264cb7a7b
BOTTE Valentin <vale.007@hotmail.fr>
2016-12-02 11:53:03 +0100

    debut interface

--- index.php
+++ index.php
@@ -1,10 +1,54 @@
 <!DOCTYPE html>
+
 <html>
+
   <head>
-    <meta charset="utf-8">
-    <title> Gestion des demandes d'accès au wifi </title>
+	  	<title>Connexion</title>
+	    <meta charset="utf-8">
+
+	     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
   </head>
+
   <body>
-    <p> coucou </p>
+    <div class="container">
+
+	
+	   		<h5 class="center-align">
+	   		 	<img width="200" src="images/logo.png" />
+</h5>
+			 <div class="row">
+
+			    <form class="col s12">
+
+			     <div class="row">
+
+			        <div class="input-field col s4  offset-s4">
+			          <input id="ndc" type="text" class="validate">
+			          <label for="ndc">Nom de compte</label>
+			        </div>
+
+			     </div>
+
+			    <div class="row">
+			        <div class="input-field col s4 offset-s4">
+			          <input id="mdp" type="password" class="validate">
+			          <label for="mdp">Mot de passe</label>
+			        </div>
+
+			     </div>
+
+			     </div>
+			    </form>
+		
+
+	   		
+
+   
+    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
   </body>
+
 </html>
