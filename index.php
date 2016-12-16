bfb814bd21853f2916fe9543454f22099de9d4fb
BOTTE Valentin <vale.007@hotmail.fr>
2016-12-16 10:48:21 +0100

    Ajout submit btn

--- index.php
+++ index.php
@@ -6,46 +6,41 @@
 	  	<title>Connexion</title>
 	    <meta charset="utf-8">
 
-	     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+	     <link rel="stylesheet" href="css/materialize.css">
   </head>
 
   <body>
+
     <div class="container">
 
-	
 	   		<h5 class="center-align">
 	   		 	<img width="200" src="images/logo.png" />
-</h5>
+			</h5>
+
 			 <div class="row">
 
 			    <form class="col s12">
+				     <div class="row">
+				        <div class="input-field col s4  offset-s4">
+				          <input id="ndc" type="text">
+				          <label for="ndc">Nom de compte</label>
+				        </div>
+				     </div>
 
-			     <div class="row">
+				    <div class="row">
+				        <div class="input-field col s4 offset-s4">
+				          <input id="mdp" type="password">
+				          <label for="mdp">Mot de passe</label>
+				        </div>
+				     </div>
 
-			        <div class="input-field col s4  offset-s4">
-			          <input id="ndc" type="text" class="validate">
-			          <label for="ndc">Nom de compte</label>
-			        </div>
+					<div class="center-align">
+ 						<button class="btn waves-effect waves-light" type="submit" name="action">CONNEXION</button>
+ 					</div>
 
-			     </div>
-
-			    <div class="row">
-			        <div class="input-field col s4 offset-s4">
-			          <input id="mdp" type="password" class="validate">
-			          <label for="mdp">Mot de passe</label>
-			        </div>
-
-			     </div>
-
-			     </div>
-			    </form>
-		
-
-	   		
-
-   
-    </div>
-
+			</div>
+	</form>
+	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 
