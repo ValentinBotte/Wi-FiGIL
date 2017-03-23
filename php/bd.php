323bd4ab62a887e18d14f13a58377627605fe7d3
BOTTE <vale.007@hotmail.fr>
2017-03-23 22:12:04 +0100

    Correction UTF8 PDO et icone

--- php/bd.php
+++ php/bd.php
@@ -3,7 +3,7 @@
     require_once('config.php');
 
     try {
-        $bd = new PDO($host . ';dbname=' . $nomBase, $ndc, $mdp);
+        $bd = new PDO($host . ';dbname=' . $nomBase, $ndc, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
